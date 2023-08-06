fb00072c7c6f099ab154b00cb7d878994b9574c0
Jin-hyeok2 <hjhlove1120@gmail.com>
2023-08-06 23:43:58 +0900

    기본 구조 설정 및 예시 코드 작성

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ props.text }}</h1>
+    <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue";
+
+const props = defineProps({
+  text: {
+    type: String,
+    required: false,
+    default: "header",
+  },
+});
+
+const emit = defineEmits(["renew"])
+
+const sendEvent = () => {
+  emit("renew")
+}
+</script>
+
+<style scoped></style>
